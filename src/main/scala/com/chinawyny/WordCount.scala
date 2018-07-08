78faa3c77ca6bc5480f961bc3e10c021f5c42bd7
fangwei <979629903@qq.com>
2018-07-08 10:22:09 +0800

    过滤空字符串

--- src/main/scala/com/chinawyny/WordCount.scala
+++ src/main/scala/com/chinawyny/WordCount.scala
@@ -1,15 +1,17 @@
 package com.chinawyny
 
+import org.apache.log4j.{Level, Logger}
 import org.apache.spark.{SparkConf, SparkContext}
 
 object WordCount {
     def main(args: Array[String]) {
 
+        Logger.getLogger("org").setLevel(Level.WARN)
         val conf = new SparkConf().setAppName("WordCount").setMaster("local")
         val sc = new SparkContext(conf)
-        val textFile = sc.textFile("file:///C:/Users/fangw/IdeaProjects/spark-demo/text.txt")
+        val textFile = sc.textFile("text.txt")
         val words = textFile.flatMap(line => line.split(" "))
-        val wordPairs = words.map(word => (word, 1))
+        val wordPairs = words.filter(!_.equals(""))map(word => (word, 1))
         val wordCounts = wordPairs.reduceByKey((a,b) => a + b)
         println("wordCounts: ")
         wordCounts.collect().foreach(println)
