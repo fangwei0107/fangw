fccef85494efcc8881da3f9fa695d370252f917a
fangwei0107 <979629903@qq.com>
2018-08-16 11:14:24 +0800

    算法

---
+++ src/main/scala/com/chinawyny/grammer/ArrayDemo.scala
@@ -0,0 +1,16 @@
+package com.chinawyny.grammer
+
+import java.util
+
+import scala.util.Sorting
+
+
+object ArrayDemo {
+
+    def main(args: Array[String]): Unit = {
+        val res = BigDecimal(ArrayData.data1) - ArrayData.data2
+        println(Array[Byte]())
+        println(ArrayData.arr.getClass)
+    }
+
+}
